b5e9e5662e24280f04ccb1400ab5a0b517d5353d
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2021-06-01 21:52:49 +0800

    add notices.nav

--- app/controllers/admin/notices_controller.go
+++ app/controllers/admin/notices_controller.go
@@ -3,6 +3,7 @@ package admin
 import (
 	"goerrbit/app/models"
 	"goerrbit/app/serializers"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 )
@@ -16,6 +17,7 @@ func init() {
 func (c noticesCtrl) init(g *echo.Group) {
 	g.GET("/apps/:app_id/problems/:problem_id/notices", c.list)
 	g.GET("/apps/:app_id/problems/:problem_id/notices/:id", c.show)
+	g.GET("/apps/:app_id/problems/:problem_id/notices/:id/nav", c.nav)
 }
 
 func (ctrl noticesCtrl) list(c echo.Context) error {
@@ -34,6 +36,26 @@ func (ctrl noticesCtrl) show(c echo.Context) error {
 	return c.JSON(200, struct {
 		Notice serializers.AdminNotice
 	}{serializers.NewAdminNotice(notice)})
+}
+
+func (ctrl noticesCtrl) nav(c echo.Context) error {
+	_, problem := ctrl.findAppProblem(c)
+	var nav struct {
+		Number *int
+		Older  *int
+		Newer  *int
+	}
+	sql := `SELECT number, older, newer from (
+SELECT id, ROW_NUMBER() OVER (ORDER BY id ASC) number,
+LEAD(notices.id) OVER (order BY id DESC) older,
+LAG(notices.id) OVER (ORDER BY id DESC) newer FROM notices WHERE problem_id = $1
+) a WHERE id = $2`
+	sql = strings.Replace(sql, "\n", " ", -1)
+	c.(Ctx).ModelNotice.NewSQLWithValues(sql, problem.Id, c.Param("id")).
+		MustQueryRow(&nav.Number, &nav.Older, &nav.Newer)
+	return c.JSON(200, struct {
+		Nav interface{}
+	}{nav})
 }
 
 func (_ noticesCtrl) findAppProblem(c echo.Context) (app models.App, problem models.Problem) {
